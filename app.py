1e2c48db2c6cbe4f712f7abb9fb60bdeadf05d31
pythonyasu0706-cpu <python.yasu0706@gmail.com>
2026-04-29 16:27:29 +0900

    fix resend api and contact form

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for, request
 from flask_wtf import FlaskForm
 # forms.pyをimport
 from forms import UserInfoForm
-import smtplib
-from email.mime.text import MIMEText
-from email.utils import formatdate
+import resend
 import os
 
 # Flozen-Flask
@@ -190,62 +188,61 @@ def confirm():
 @app.route('/contact/send', methods=['POST'])
 def send():
     form = UserInfoForm()
+
     if not form.validate_on_submit():
         return redirect(url_for('contact'))
 
-    # メール送信処理
-    msg = MIMEText(f"""
+    # Resend APIキー
+    resend.api_key = os.environ.get("re_ft1cU7yP_LAL4obeAUFEFW1BHyLZgQKrY")
+
+    # 管理者宛メール
+    resend.Emails.send({
+        "from": "onboarding@resend.dev",
+        "to": ["s10ak025@gmail.com"],
+        "subject": "お問い合わせ通知",
+        "text": f"""
 名前: {form.name.data}
 メール: {form.email.data}
-内容:
+電話番号: {form.tel.data}
+住所: {form.address.data}
+
+お問い合わせ内容:
 {form.note.data}
-        """)
+"""
+    })
 
-    msg['Subject'] = 'お問い合わせ'
-    msg['From'] = os.environ.get("EMAIL_USER")
-    msg['To'] = 's10ak025@gmail.com'
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=10) as smtp:
-        smtp.login(
-            os.environ.get("EMAIL_USER"),
-            os.environ.get("EMAIL_PASS")
-        )
-
-        smtp.send_message(msg)
-
-        reply = MIMEText(f"""
+    # 自動返信メール
+    resend.Emails.send({
+        "from": "onboarding@resend.dev",
+        "to": [form.email.data],
+        "subject": "【福岡観光協会】お問い合わせ受付完了",
+        "text": f"""
 {form.name.data} 様
 
-このたびは福岡観光協会のお問い合わせフォームよりご連絡いただき、
-誠にありがとうございます。
+このたびは福岡観光協会のお問い合わせフォームより
+お問い合わせいただきありがとうございます。
 
-以下の内容でお問い合わせを受け付けいたしました。
+以下の内容で受け付けました。
 
---------------------
-■お名前：{form.name.data}
-■メールアドレス：{form.email.data}
-■お問い合わせ内容：
+-------------------------
+資料請求: {', '.join(form.catalog.data) if form.catalog.data else 'なし'}
+件名: {form.title.data}
+お名前: {form.name.data}
+メール: {form.email.data}
+電話番号: {form.tel.data}
+住所: {form.address.data}
+お問い合わせ内容:
 {form.note.data}
---------------------
+-------------------------
 
-内容を確認のうえ、担当者より順次ご返信させていただきます。
-なお、内容によってはご返信まで数日いただく場合がございます。
+内容確認後、担当者よりご連絡いたします。
+しばらくお待ちください。
 
-あらかじめご了承くださいますようお願い申し上げます。
-
-────────────────────
 福岡観光協会
-お問い合わせ窓口（自動返信メール）
-────────────────────
-""")
-        reply['Subject'] = '【福岡観光協会】お問い合わせ・資料請求受付完了のお知らせ'
-        reply['From'] = os.environ.get("EMAIL_USER")
-        reply['To'] = form.email.data
+"""
+    })
 
-        smtp.send_message(reply)
-    
     return redirect(url_for('result'))
-
 
 # 完了
 @app.route('/contact/result', methods=['GET'])
