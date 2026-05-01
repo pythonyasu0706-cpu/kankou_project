e882d80c183e50ab4160e70070a961deaa26ab5e
pythonyasu0706-cpu <python.yasu0706@gmail.com>
2026-05-01 23:32:33 +0900

    不要なフォルダを削除

--- 0428旧/kankou_project3/static/js/main.js
+++
@@ -1,9 +0,0 @@
-// static/js/main.js
-// ハンバーガーメニューの動作
-const hamburger = document.getElementById("hamburger");
-const nav = document.getElementById("nav");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    nav.classList.toggle("active");
-});
